ccf5bfd05f8b1e99cc792fc9343d5774f25c1ac0
Keith Watt <kwatt@earthintegate.com>
2020-01-23 11:42:43 -0600

    first

---
+++ img-resizer/winMain.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace img_resizer
+{
+    partial class winMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(winMain));
+            this.btnResize = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dlgBrowseDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtW = new System.Windows.Forms.TextBox();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.rdoAbs = new System.Windows.Forms.RadioButton();
+            this.rdoPerc = new System.Windows.Forms.RadioButton();
+            this.txtPerc = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnResize
+            // 
+            this.btnResize.Location = new System.Drawing.Point(368, 396);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(75, 23);
+            this.btnResize.TabIndex = 0;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 92);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Image Directory:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(9, 32);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(386, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(12, 129);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(220, 290);
+            this.lbFiles.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Files to be resized:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "W:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtPerc);
+            this.groupBox2.Controls.Add(this.rdoPerc);
+            this.groupBox2.Controls.Add(this.rdoAbs);
+            this.groupBox2.Controls.Add(this.txtPrefix);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtH);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtW);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(238, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(205, 228);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resize Options";
+            // 
+            // txtW
+            // 
+            this.txtW.Location = new System.Drawing.Point(39, 50);
+            this.txtW.Name = "txtW";
+            this.txtW.Size = new System.Drawing.Size(58, 20);
+            this.txtW.TabIndex = 5;
+            this.txtW.Text = "1024";
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(136, 50);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(58, 20);
+            this.txtH.TabIndex = 7;
+            this.txtH.Text = "768";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "H:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "New File Prefix:";
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(6, 164);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(179, 20);
+            this.txtPrefix.TabIndex = 9;
+            this.txtPrefix.Text = "resized_";
+            // 
+            // rdoAbs
+            // 
+            this.rdoAbs.AutoSize = true;
+            this.rdoAbs.Location = new System.Drawing.Point(6, 27);
+            this.rdoAbs.Name = "rdoAbs";
+            this.rdoAbs.Size = new System.Drawing.Size(89, 17);
+            this.rdoAbs.TabIndex = 10;
+            this.rdoAbs.Text = "Absolute Size";
+            this.rdoAbs.UseVisualStyleBackColor = true;
+            // 
+            // rdoPerc
+            // 
+            this.rdoPerc.AutoSize = true;
+            this.rdoPerc.Checked = true;
+            this.rdoPerc.Location = new System.Drawing.Point(6, 86);
+            this.rdoPerc.Name = "rdoPerc";
+            this.rdoPerc.Size = new System.Drawing.Size(80, 17);
+            this.rdoPerc.TabIndex = 11;
+            this.rdoPerc.TabStop = true;
+            this.rdoPerc.Text = "Percentage";
+            this.rdoPerc.UseVisualStyleBackColor = true;
+            // 
+            // txtPerc
+            // 
+            this.txtPerc.Location = new System.Drawing.Point(39, 109);
+            this.txtPerc.Name = "txtPerc";
+            this.txtPerc.Size = new System.Drawing.Size(58, 20);
+            this.txtPerc.TabIndex = 12;
+            this.txtPerc.Text = "0.25";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "%:";
+            // 
+            // winMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 426);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbFiles);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnResize);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "winMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Resizer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog dlgBrowseDir;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtW;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPerc;
+        private System.Windows.Forms.RadioButton rdoPerc;
+        private System.Windows.Forms.RadioButton rdoAbs;
+    }
+}
+
---
+++ img-resizer/winMain.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace img_resizer
+{
+    public partial class winMain : Form
+    {
+        public winMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+
+            if (lbFiles.Items.Count == 0)
+                return;
+            if (txtH.Text.Length == 0)
+                return;
+            if (txtW.Text.Length == 0)
+                return;
+            if (txtPrefix.Text.Length == 0)
+                return;
+
+
+            btnResize.Enabled = false;
+            Application.DoEvents();
+
+            foreach (var f in Directory.EnumerateFiles(txtPath.Text))
+            {
+                if (!f.ToLower().Contains(".jpg"))
+                    continue;
+
+                FileInfo info = new FileInfo(f);
+                using (Image image = Image.FromFile(f))
+                {
+
+                    if (rdoPerc.Checked)
+                    {
+                        using (Bitmap resizedImage = ImageHelper.ResizeImage(image, decimal.Parse(txtPerc.Text)))
+                        {
+                            resizedImage.Save(
+                                info.DirectoryName + "\\"
+                                    + txtPrefix.Text
+                                    + info.Name.Substring(0, info.Name.LastIndexOf(info.Extension))
+                                    + "_" + resizedImage.Width + "_" + resizedImage.Height
+                                    + info.Extension,
+                                ImageFormat.Jpeg);
+                        }
+                    }
+                    else
+                    {
+                        using (Bitmap resizedImage = ImageHelper.ResizeImage(image, int.Parse(txtW.Text), int.Parse(txtH.Text)))
+                        {
+                            resizedImage.Save(
+                                info.DirectoryName + "\\"
+                                    + txtPrefix.Text
+                                    + info.Name.Substring(0, info.Name.LastIndexOf(info.Extension))
+                                    + "_" + resizedImage.Width + "_" + resizedImage.Height
+                                    + info.Extension,
+                                ImageFormat.Jpeg);
+                        }
+                    }
+                   
+                }
+
+                foreach(var item in lbFiles.Items)
+                {
+                    if (f.ToLower().Contains(item.ToString().ToLower()))
+                    {
+                        lbFiles.Items.Remove(item);
+                        Application.DoEvents();
+                        break;
+                    }
+                }
+            }
+
+            btnResize.Enabled = true;
+            MessageBox.Show("Operation completed successfully", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dlgBrowseDir.ShowDialog();
+            txtPath.Text = dlgBrowseDir.SelectedPath;
+            list_files();
+        }
+
+        private void list_files()
+        {
+            lbFiles.Items.Clear();
+            
+            foreach (var f in Directory.EnumerateFiles(txtPath.Text))
+            {
+                if (!f.ToLower().Contains(".jpg"))
+                    continue;
+                lbFiles.Items.Add(Path.GetFileName(f));                
+            }
+        }
+    }
+
+    public static class ImageHelper
+    {
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);                    
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            destImage.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            return destImage;
+        }
+
+        public static Bitmap ResizeImage(Image image, decimal percentage)
+        {
+            int width = (int)Math.Round(image.Width * percentage, MidpointRounding.AwayFromZero);
+            int height = (int)Math.Round(image.Height * percentage, MidpointRounding.AwayFromZero);
+            return ResizeImage(image, width, height);
+        }
+    }
+
+}
